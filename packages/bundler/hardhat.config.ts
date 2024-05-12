aca2cb1ccc3ff750d3ebc07bfbd4be61d5d9139e
sh kim <kim03208@naver.com>
2024-05-12 23:49:03 +0900

    WIP (connect sepolia)

--- packages/bundler/hardhat.config.ts
+++ packages/bundler/hardhat.config.ts
@@ -1,52 +1,61 @@
-import '@nomiclabs/hardhat-ethers'
-import '@nomicfoundation/hardhat-toolbox'
-import 'hardhat-deploy'
+import '@nomiclabs/hardhat-ethers';
+import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-deploy';
 
-import fs from 'fs'
+import fs from 'fs';
 
-import { HardhatUserConfig } from 'hardhat/config'
-import { NetworkUserConfig } from 'hardhat/src/types/config'
+import { HardhatUserConfig } from 'hardhat/config';
+import { NetworkUserConfig } from 'hardhat/src/types/config';
 
-const mnemonicFileName = process.env.MNEMONIC_FILE
-let mnemonic = 'test '.repeat(11) + 'junk'
+const mnemonicFileName = process.env.MNEMONIC_FILE;
+let mnemonic = 'test '.repeat(11) + 'junk';
 if (mnemonicFileName != null && fs.existsSync(mnemonicFileName)) {
-  mnemonic = fs.readFileSync(mnemonicFileName, 'ascii').trim()
+  mnemonic = fs.readFileSync(mnemonicFileName, 'ascii').trim();
 }
 
-const infuraUrl = (name: string): string => `https://${name}.infura.io/v3/${process.env.INFURA_ID}`
+const infuraUrl = (name: string): string => `https://${name}.infura.io/v3/${process.env.INFURA_ID}`;
 
-function getNetwork (url: string): NetworkUserConfig {
+function getNetwork(url: string): NetworkUserConfig {
+  console.log(`mnemonic: ${mnemonic}`);
   return {
     url,
     accounts: {
-      mnemonic
-    }
-  }
+      mnemonic,
+    },
+  };
 }
 
-function getInfuraNetwork (name: string): NetworkUserConfig {
-  return getNetwork(infuraUrl(name))
+function getInfuraNetwork(name: string): NetworkUserConfig {
+  console.warn(`name: ${name}`);
+  return getNetwork(infuraUrl(name));
 }
+
+console.warn(`process.env.INFURA_API_KEY: ${process.env.INFURA_API_KEY}`);
+console.warn(`process.env.SEPOLIA_PRIVATE_KEY: ${process.env.SEPOLIA_PRIVATE_KEY}`);
 
 const config: HardhatUserConfig = {
   typechain: {
     outDir: 'src/types',
-    target: 'ethers-v5'
+    target: 'ethers-v5',
   },
   networks: {
     localhost: {
       url: 'http://localhost:8545/',
-      saveDeployments: false
+      saveDeployments: false,
     },
-    goerli: getInfuraNetwork('goerli')
+    sepolia: {
+      url: `https://ethereum-sepolia-rpc.publicnode.com`,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY || ''],
+    },
+    goerli: getInfuraNetwork('goerli'),
   },
   solidity: {
     version: '0.8.23',
     settings: {
       evmVersion: 'paris',
-      optimizer: { enabled: true }
-    }
-  }
-}
+      optimizer: { enabled: true },
+    },
+  },
+};
 
-export default config
+export default config;
